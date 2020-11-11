28cc32963c4a8b03fa94aa67ab28f7b96cc7b6ba
Beamm <Beamm0613@163.com>
2020-11-11 16:05:51 +0800

    增加：创建资讯接口,添加图片 修改：取消DEBUG的显示

--- src/main/java/com/pojo/Popularizations.java
+++ src/main/java/com/pojo/Popularizations.java
@@ -2,6 +2,7 @@ package com.pojo;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,4 +23,6 @@ public class Popularizations {
     private Date p_createtime;
     @JsonIgnore
     private Date p_updatetime;
+    @ApiModelProperty("如果没有图片可以不传")
+    private String p_image;
 }
