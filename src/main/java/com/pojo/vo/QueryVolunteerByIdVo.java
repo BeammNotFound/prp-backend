1a2c760b4d4086b91bd68d7a9ca5a1fcaf1bcd4e
Beamm <Beamm0613@163.com>
2020-12-10 18:26:05 +0800

    update: change：根据用户id查询用户报名志愿者接口

--- src/main/java/com/pojo/vo/QueryVolunteerByIdVo.java
+++ src/main/java/com/pojo/vo/QueryVolunteerByIdVo.java
@@ -1,18 +1,17 @@
 package com.pojo.vo;
 
-import com.pojo.VolunteerInfo;
+import com.pojo.Bases;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import java.util.Date;
 
 @Data
-public class QueryVolunteerByIdVo extends VolunteerInfo {
+public class QueryVolunteerByIdVo extends Bases {
     @NotBlank(message = "用户id不能为空")
     private Integer user_id;
 
     private String av_status;
     private Date av_pass_time;
     private Date av_application_time;
-    private Integer base_id;
 }
