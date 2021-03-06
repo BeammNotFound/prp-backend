5e307c9c14a7ea633090971017fa9e3e4620d748
Beamm <Beamm0613@163.com>
2021-03-06 14:12:38 +0800

    update：根据ap_status查询领养宠物表单信息2 change：

--- src/main/java/com/pojo/vo/ApFormVo.java
+++ src/main/java/com/pojo/vo/ApFormVo.java
@@ -2,17 +2,21 @@ package com.pojo.vo;
 
 import lombok.Data;
 
+import java.util.Date;
+
 /**
  * @author BeamStark
  * @Version 0.1 2020/12
  */
 @Data
 public class ApFormVo{
+    private Integer ap_id;
     private Integer status;
     private String user_name;
     private String b_name;
     private String p_name;
     private String user_phone;
     private String ap_status;
-    private String ap_pass_time;
+    private Date ap_pass_time;
+    private Date ap_application_time;
 }
--- src/main/java/com/pojo/vo/ApStatusVo.java
+++ src/main/java/com/pojo/vo/ApStatusVo.java
@@ -11,7 +11,6 @@ import java.util.Date;
  * @Version 0.1 2020/12
  */
 @Data
-
 public class ApStatusVo {
     private Integer ap_id;
     @ApiModelProperty("审批状态： 1 审批通过; 2 待审批; 3 审批驳回")
